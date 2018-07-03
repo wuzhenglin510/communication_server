6e0d40a616c96c0aaaef44aef512327a9278044b
leo <wuzhenglin510@163.com>
2018-07-03 16:23:24 +0800

    work on

--- src/handler/article.list/index.js
+++ src/handler/article.list/index.js
@@ -3,7 +3,6 @@ const Article = require("../../model/article")
 module.exports = async (ctx) => {
     let params = ctx.request.body;
     let condition = {};
-    let currUserId = await Common.utils.retrieved_user_id(params.token)
     if (params.classify) condition.classify = params.classify;
     let articles = await Article.find(condition).sort({createTime: -1});
     if (articles)
